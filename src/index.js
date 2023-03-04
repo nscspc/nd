0ae9e0f8a5f5b6da1696c70369c390ec0a33d342
Naveen Saini <saininaveen933@gmail.com>
2023-03-04 22:05:56 +0530

    ch

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const app = express();// app is an object of express.
 const quotes = require("./quotes.json");
 const noteRouter = require("./routes/noteRoutes");
 const userRouter = require("./routes/userRoutes");
-const cors = require("cors");
-const dotenv = require("dotenv");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
 
 
 app.get("/", (req,res) => {
@@ -31,37 +31,41 @@ app.get("/randomquote", (req,res) => {
     res.status(200).json(quote);
 })
 
+app.listen(5000 , ()=>{
+            console.log("Server started at port no. "+PORT);
+        });
+
 //--------------------------------------------------------
 
-dotenv.config();
+// dotenv.config();
 
-const mongoose = require("mongoose");// mongoose is a package which is used to connect to mongodb.
+// const mongoose = require("mongoose");// mongoose is a package which is used to connect to mongodb.
 
-app.use(express.json());// it is used to pass the request body into json form., so that we can access the response directly.
+// app.use(express.json());// it is used to pass the request body into json form., so that we can access the response directly.
 
-app.use(cors());// cors() adds header to the response from the api , so that api can be called from every place.
+// app.use(cors());// cors() adds header to the response from the api , so that api can be called from every place.
 
-app.use((req,res,next)=>{
-    console.log("HTTP Method - "+ req.method+ ",URL - "+ req.url);
-    next();
-});
+// app.use((req,res,next)=>{
+//     console.log("HTTP Method - "+ req.method+ ",URL - "+ req.url);
+//     next();
+// });
 
-app.use("/users",userRouter);
-app.use("/note",noteRouter);
+// app.use("/users",userRouter);
+// app.use("/note",noteRouter);
 
-app.get("/",(req,res)=>{
-    res.send("Notes Api");
-})
+// app.get("/",(req,res)=>{
+//     res.send("Notes Api");
+// })
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-mongoose.connect("mongodb+srv://saininaveen933:naveen1234@cluster0.0gsvxzs.mongodb.net/?retryWrites=true&w=majority")//("mongodb+srv://saininaveen933:naveen1234@cluster0.0gsvxzs.mongodb.net/?retryWrites=true&w=majority")
-.then(()=>{ //Here then( ) is a callback function which is executed when the connection is successful in this case. And , if any error then catch( ) function is executed.
-    app.listen(PORT , ()=>{
-        console.log("Server started at port no. "+PORT);
-    });
-    // exports.app = functions.https.onRequest(app);
-})
-.catch((error)=>{
-    console.log(error);
-})
+// mongoose.connect("mongodb+srv://saininaveen933:naveen1234@cluster0.0gsvxzs.mongodb.net/?retryWrites=true&w=majority")//("mongodb+srv://saininaveen933:naveen1234@cluster0.0gsvxzs.mongodb.net/?retryWrites=true&w=majority")
+// .then(()=>{ //Here then( ) is a callback function which is executed when the connection is successful in this case. And , if any error then catch( ) function is executed.
+//     app.listen(PORT , ()=>{
+//         console.log("Server started at port no. "+PORT);
+//     });
+//     // exports.app = functions.https.onRequest(app);
+// })
+// .catch((error)=>{
+//     console.log(error);
+// })
